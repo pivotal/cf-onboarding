e5128a0ed10591e79bfbb0edadb0a2cfbba03cf9
Supraja Narasimhan <snarasimhan@pivotal.io>
2020-05-13 15:10:59 -0700

    Explain input for Onboarding env cleanup script
    - Describe the input argument to the cleanup script and how to obtain it.
    
    [#172824388]

--- scripts/clean_up_gcp_project.sh
+++ scripts/clean_up_gcp_project.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-# Each service account key is tied to a single GCP project.
+#### USAGE
+# ./clean_up_gcp_project.sh <PATH TO SERVICE ACCOUNT KEY>
+
+#### HOW TO FIND FILE PATH TO SERVICE ACCOUNT KEY
+# 1. Navigate to the list of service accounts for the GCP Project you want to delete.
+# 2. Click on one and choose "+ Create Key" to create a JSON key. This will
+# download the file containing the key to your workstation.
+#    - There may be multiple service accounts for a single GCP Project. 
+#    - The [default serviceaccount](https://cloud.google.com/iam/docs/service-accounts#default) 
+#      has a large number of permissions and may be a good choice.
+#     - Consider updating this document with instructions on how to identify a default service account.
+# 3. Copy the path to the file.
+
+display_usage() {
+  echo "Please provide the one required argument: the file path to a service account key for the target CF Onboarding Env's GCP Project."
+}
+
+# if less than one argument supplied, display usage
+if [  $# -lt 1 ]
+then
+  display_usage
+  exit 1
+fi
+
 export BBL_GCP_SERVICE_ACCOUNT_KEY=$(cat $1)
 export BBL_GCP_REGION=us-west1
 export BBL_IAAS=gcp
 
-bbl cleanup-leftovers --no-confirm
+read -p "Are you sure? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  eacho "Starting to clean up GCP resources ..."
+  bbl cleanup-leftovers --no-confirm
+  echo "Finished cleaning up GCP resources."
+fi
