7d603a239e27a9c08e7cf9e877ccc5ec726db277
Supraja Narasimhan <snarasimhan@pivotal.io>
2020-05-13 15:22:18 -0700

    Fix broken output for GCP env cleanup script
    - Fix typo of "echo" command.
    - Clarify script output by adding line break
    - Improve accuracy of instructions for getting input argument for
    script.
    
     [#172824388]

--- scripts/clean_up_gcp_project.sh
+++ scripts/clean_up_gcp_project.sh
@@ -4,8 +4,8 @@
 # ./clean_up_gcp_project.sh <PATH TO SERVICE ACCOUNT KEY>
 
 #### HOW TO FIND FILE PATH TO SERVICE ACCOUNT KEY
-# 1. Navigate to the list of service accounts for the GCP Project you want to delete.
-# 2. Click on one and choose "+ Create Key" to create a JSON key. This will
+# 1. Navigate "IAM & Admin" > "Service Accounts" for the GCP Project you want to delete.
+# 2. Click on one and choose "Edit", then "+ Create Key" to create a "JSON" key. This will
 # download the file containing the key to your workstation.
 #    - There may be multiple service accounts for a single GCP Project. 
 #    - The [default serviceaccount](https://cloud.google.com/iam/docs/service-accounts#default) 
@@ -31,7 +31,7 @@ export BBL_IAAS=gcp
 read -p "Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  eacho "Starting to clean up GCP resources ..."
+  echo "\nStarting to clean up GCP resources ..."
   bbl cleanup-leftovers --no-confirm
   echo "Finished cleaning up GCP resources."
 fi
